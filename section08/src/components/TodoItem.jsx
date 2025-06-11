53ad875fedff5e2646d461ef539da7acd0cdb7d5
Jayden <jayden6849@gmail.com>
2025-06-11 17:19:14 +0900

    [Jayden] feat: 투두리스트에 항목 추가

---
+++ section08/src/components/TodoItem.jsx
@@ -0,0 +1,14 @@
+import './TodoItem.css';
+
+const TodoItem = ({id, isDone, content, date}) => {
+    return (
+        <div className="todo-item">
+            <input type="checkbox" checked={isDone} readOnly />
+            <div className="content">{content}</div>
+            <div className="date">{new Date(date).toLocaleDateString()}</div>
+            <button>삭제</button>
+        </div>
+    )
+};
+
+export default TodoItem;
