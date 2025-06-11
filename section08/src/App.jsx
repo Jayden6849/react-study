43d78fa229f79d5b7b5de863b6459b58ae876ee9
Jayden <jayden6849@gmail.com>
2025-06-11 17:51:04 +0900

    [Jayden] feat: 투두리스트 삭제 구현현

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -6,10 +6,10 @@ import Editor from './components/Editor';
 import List from './components/List';
 
 const mockData = [
-  {
-    id: 0,
+    {
+    id: 2,
     isDone: false,
-    content: "React 공부하기",
+    content: "노래 연습하기",
     date: new Date().getTime(),
   },
   {
@@ -19,9 +19,9 @@ const mockData = [
     date: new Date().getTime(),
   },
   {
-    id: 2,
+    id: 0,
     isDone: false,
-    content: "노래 연습하기",
+    content: "React 공부하기",
     date: new Date().getTime(),
   },
 ];
@@ -44,17 +44,21 @@ function App() {
 
   const onUpdate = (targetId) => {
     // todos State 값을 중 targetId 와 일치하는 id를 갖는 투두 아이템의 isDone 을 변경
-
     setTodos(todos.map((todo)=>
       todo.id === targetId ? {...todo, isDone:!todo.isDone} : todo
     ));
+  };
+
+  const onDelete = (targetId) => {
+    // todos State 값을 중 targetId 와 일치하는 id를 갖는 요소를 삭제한 새로운 배열을 반환
+    setTodos(todos.filter((todo) => todo.id !== targetId));
   };
 
   return (
     <div className="app">
       <Header />
       <Editor onCreate={onCreate}/>
-      <List todos={todos} onUpdate={onUpdate}/>
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete}/>
     </div>
   )
 }
--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -3,7 +3,7 @@ import TodoItem from "./TodoItem";
 
 import {useState} from 'react';
 
-const List = ({todos, onUpdate}) => {
+const List = ({todos, onUpdate, onDelete}) => {
 
     const[search, setSearch] = useState("");
 
@@ -29,7 +29,7 @@ const List = ({todos, onUpdate}) => {
             <input value={search} onChange={onChangeSearch} placeholder="검색어를 입력하세요."/>
             <div className="todos-wrapper">
                 {filteredTodos.map((todo)=>{
-                    return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} />;
+                    return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} onDelete={onDelete} />;
                 })}
             </div>
         </div>
