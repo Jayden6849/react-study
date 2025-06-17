dcf66db2a4ca926a186e58d1daba3861b7dc6cb2
Jayden <jayden6849@gmail.com>
2025-06-17 11:58:54 +0900

    [Jayden] feat: 동적 라우팅 - URL 파라미터, 쿼리 스트링

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,4 +1,11 @@
+import { useSearchParams } from "react-router-dom";
+
 const Home = () => {
+    const [params, setParams] = useSearchParams();
+
+    // params.get("value");
+    // -> localhost:5173/?value=____ : 해당하는 값을 가리키게 됨
+
     return (
         <div>
             HomePage
