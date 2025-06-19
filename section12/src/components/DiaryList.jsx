2e55952b6cb0a446645d6acd5cd5e84b89d186f1
Jayden <jayden6849@gmail.com>
2025-06-19 09:37:12 +0900

    [Jayden] style: 쓰이지 않는 클래스명 변경

--- section12/src/components/DiaryList.jsx
+++ section12/src/components/DiaryList.jsx
@@ -27,7 +27,7 @@ const DiaryList = ({data}) => {
 
     return (
         <div className="DiaryList">
-            <div className="menuBar">
+            <div className="menu_bar">
                 <select onChange={onChangeSortType}>
                     <option value={"latest"}>최신순</option>
                     <option value={"oldest"}>오래된 순</option>
